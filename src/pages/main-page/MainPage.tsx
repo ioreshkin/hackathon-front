e725801ccab27ee2163fb566eddc3d9e444f8067
Ilya <fennonplay@gmail.com>
2025-05-22 02:20:44 +0500

    Dockerfile update

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -3,12 +3,12 @@ import {request} from "../../services/request.ts";
 
 const MainPage = () => {
 
-  request('/lol/lol').catch(res => console.log(res));
-  request('/lol').catch(res => console.log(res));
+  request('/api/lol/lol').catch(res => console.log(res));
+  request('/api/lol').catch(res => console.log(res));
 
 
-  request('/lol/lol').then(res => console.log(res));
-  request('/lol').then(res => console.log(res));
+  request('/api/lol/lol').then(res => console.log(res));
+  request('/api/lol').then(res => console.log(res));
 
 
   return (
