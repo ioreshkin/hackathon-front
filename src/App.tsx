d03b8ab86efe98fd51b80e868aa177fe2611f9a3
Ilya <fennonplay@gmail.com>
2025-05-22 01:49:37 +0500

    Dockerfile update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Provider store={store}>
         <Router>
           <Routes>
-            <Route path="/" element={<MainPage/>} />
+            <Route path="/*" element={<MainPage/>} />
           </Routes>
         </Router>
       </Provider>
