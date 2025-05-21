bb4898789c04b58aca21e0c41a8e201cd8251ceb
Ilya <fennonplay@gmail.com>
2025-05-21 23:40:09 +0500

    Dockerfile update

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -1,6 +1,10 @@
 import styles from './main-page.module.css'
+import {request} from "../../services/request.ts";
 
 const MainPage = () => {
+
+  request('/lol').then(res => console.log(res));
+
   return (
       <div className={styles.container}>
         ЭЩКЕРЕ
