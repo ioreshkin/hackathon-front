7add321d6ffc87eb753de4d2134a44f26654c730
Ilya <fennonplay@gmail.com>
2025-05-23 11:48:53 +0500

    finish

--- src/components/emergency-reports/EmergencyReports.tsx
+++ src/components/emergency-reports/EmergencyReports.tsx
@@ -21,7 +21,7 @@ const EmergencyReports = () => {
     setCount(count + 5);
   };
 
-  if (!events || !emergencyReport) return null;
+  if (!events || !emergencyReport || emergencyReport.events.length < 1) return null;
 
   return (
       <div className={styles.container}>
